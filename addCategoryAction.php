5df4a0250e45fb7c971fd4906bd3f8473ae4039e
MhmdAlRawass <mhmdalrawass12@gmail.com>
2025-01-05 17:15:55 +0200

    Mobile

--- addCategoryAction.php
+++ addCategoryAction.php
@@ -1,43 +1,29 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-$con = mysqli_connect("localhost", "root", "", "todo");
+$con = mysqli_connect("fdb1027.runhosting.com", "4571066_todo", "Rawass123456", "4571066_todo");
 
 if (mysqli_connect_errno()) {
     echo json_encode(['status' => 'error', 'message' => 'Failed to connect to MySQL: ' . mysqli_connect_error()]);
     exit();
 }
 
-$taskId = isset($_GET['taskId']) ? (int) $_GET['taskId'] : null;
-$isDoneBool = isset($_GET['isDone']) ? $_GET['isDone'] : null;
-$categoryId = isset($_GET['categoryId']) ? (int) $_GET['categoryId'] : null;
+$name = isset($_GET['name']) ;
+$color = isset($_GET['color']);
 
-if ($taskId === null || $isDoneBool === null || $categoryId === null) {
-    echo json_encode(['status' => 'error', 'message' => 'Missing taskId, isDone, or categoryId parameter.']);
-    exit;
+if ($name === null || $color === null) {
+    echo json_encode(['status' => 'error', 'message' => 'Missing name or color parameter.']);
+    exit();
 }
 
-$isDone = ($isDoneBool === 'true') ? 1 : 0;
-
-$sql = "UPDATE tasks SET db_isDone = $isDone WHERE db_id = $taskId AND db_categoryId = $categoryId";
+$sql = "INSERT INTO categories (db_name, db_color) VALUES ('$name', '$color')";
 if (mysqli_query($con, $sql)) {
-
-    $sqlCount = "SELECT COUNT(*) as doneCount FROM tasks WHERE db_isDone = 1 AND db_categoryId = $categoryId";
-    $result = mysqli_query($con, $sqlCount);
-
-    if ($result) {
-        $row = mysqli_fetch_assoc($result);
-        $doneCount = $row['doneCount'];
-
-        echo json_encode(['status' => 'success', 'doneCount' => $doneCount]);
-    } else {
-        echo json_encode(['status' => 'error', 'message' => 'Failed to count completed tasks in the specified category.']);
-    }
+    echo json_encode(['status' => 'success', 'message' => 'Category added successfully!']);
 } else {
-    echo json_encode(['status' => 'error', 'message' => 'Failed to update task status.']);
+    echo json_encode(['status' => 'error', 'message' => 'Failed to add category: ' . mysqli_error($con)]);
 }
 
 mysqli_close($con);
